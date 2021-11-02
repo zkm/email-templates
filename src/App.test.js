604d2bc017d3f4df475a312cfea9628b3d08b625
Zach Schneider <me@zachschneider.com>
2021-11-02 13:54:29 -0500

    react tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Template react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Template A/i);
   expect(linkElement).toBeInTheDocument();
 });
